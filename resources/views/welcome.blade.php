6ced7f17bc2cdc1b14a6f2d36103fbe809ec484a
maxcott <rentidnz@gmail.com>
2017-07-03 20:57:57 +1200

    got it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">A slightly sort of Laravel 5</div>
             </div>
         </div>
     </body>
