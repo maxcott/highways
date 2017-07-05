a9ef2c734dc16794327fcd4ba475a130cc61a3ba
Michael Wijtenburg (AT) <rentidnz@gmail.com>
2017-07-05 22:22:50 +1200

    z

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">X morphed why o why please say you'll stay</div>
+                <div class="title">Z</div>
             </div>
         </div>
     </body>
