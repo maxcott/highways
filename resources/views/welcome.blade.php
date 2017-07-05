5bc19a31ba2ad46fedac81b1a7acc63be1d1fc01
maxcott <rentidnz@gmail.com>
2017-07-05 22:02:58 +1200

    working

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">X morphed why o why please say</div>
+                <div class="title">X morphed why o why please say you'll stay</div>
             </div>
         </div>
     </body>
